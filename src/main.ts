fd5cae55c777723c5a734f77763631f345f104a2
Navya Sri Nandyala <navyasrinandyala@gmail.com>
2026-02-22 23:52:54 -0500

    bugfix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { provideRouter, withInMemoryScrolling, withHashLocation } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
@@ -10,6 +10,6 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes, withInMemoryScrolling({
       scrollPositionRestoration: 'enabled',
       anchorScrolling: 'enabled'
-    }))
+    }), withHashLocation())
   ]
 }).catch(err => console.error(err));
